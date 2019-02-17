faa888a9fa1b5ebb9491a3cac79eaaa7a92ed9c4
George Antonious <george@antonious.ca>
2019-02-17 08:59:43 -0500

    Write main sample as idiomatic xp

--- src/ca/exceptionalprogramming/xpkit/sample/main.kt
+++ src/ca/exceptionalprogramming/xpkit/sample/main.kt
@@ -4,9 +4,14 @@ import ca.exceptionalprogramming.xpkit.arithmetic.plus
 import ca.exceptionalprogramming.xpkit.core.ValueException
 import ca.exceptionalprogramming.xpkit.core.getAs
 import ca.exceptionalprogramming.xpkit.core.throwResult
+import java.lang.IllegalStateException
 
 fun main() {
-    print({ fib(10) }.getAs<Int>())
+    try {
+        fib(10)
+    } catch (ex: ValueException) {
+        print(ex.getAs<Int>())
+    }
 }
 
 fun fib(n: Int): Nothing {
